aa0343e560a5a1f798396d41797316ba17c7f969
pepentosee123 <aliyahuda33@gmail.com>
2021-10-10 15:54:07 +0300

    pertemuan 3

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Aliyah</li>
+		<li>Nama Belakang : Huda</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Jalan Ardio gang 8 </li>
+	</ul>
+
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Bogor</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Bulutangkis</li>
+	</ul>
+</section>
